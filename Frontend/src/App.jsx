b94243d031411f1411fc7dec3f49478d90525a28
mrmuhammadusman2444-cmd <mrmuhammadusman2444@gmail.com>
2026-06-28 16:15:22 +0500

    chuti

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Registration from './pages/Registration.jsx'
 import Login from './pages/Login.jsx'
 import Sidebar from './components/Sidebar.jsx'
-// import Home from './pages/Home.jsx'
 import { ToastContainer } from 'react-toastify'
 import NewSale from './pages/Sales Section/NewSale.jsx'
 import ManageSale from './pages/Sales Section/ManageSale.jsx'
@@ -30,7 +29,6 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Registration />} />
         <Route path='/login' element={<Login />} />
-        {/* <Route path='/home' element={<Home />} /> */}
         <Route path='/newSale' element={<NewSale />} />
         <Route path='/manageSale' element={<ManageSale />} />
         <Route path='/newProduct' element={<NewProduct />} />
